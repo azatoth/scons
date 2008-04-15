4feb688e4af7e13dfc04f99db3e0b0d2e205270c
Benoit Belley <belley@developer.scons.org>
2008-04-15 19:10:52 +0000

    Improved the multiple-parents.py test to also tests the following cases:
    d) Some children are ignored
    e) Some children are pre-requesites
    f) Some sources are missing
    
    The test still passes. No extra bug were found.

--- test/Parallel/multiple-parents.py
+++ test/Parallel/multiple-parents.py
@@ -48,31 +48,70 @@ test = TestSCons.TestSCons()
 #    a) Some nodes have multiple parents
 #    b) Some targets fail building
 #    c) Some targets succeed building
+#    d) Some children are ignored
+#    e) Some children are pre-requesites
+#    f) Some sources are missing
 
 test.write('SConstruct', """
 def fail_action(target = None, source = None, env = None):
     return 2
 
-failed_target0 = Command(target='failed00', source='', action=fail_action)
-ok_target0     = Command(target='ok00',     source='', action=Touch('${TARGET}'))
+failed0  = Command(target='failed00',  source='', action=fail_action)
+ok0      = Command(target='ok00',      source='', action=Touch('${TARGET}'))
+prereq0  = Command(target='prereq00',  source='', action=Touch('${TARGET}'))
+ignore0  = Command(target='ignore00',  source='', action=Touch('${TARGET}'))
+igreq0   = Command(target='igreq00',   source='', action=Touch('${TARGET}'))
+missing0 = Command(target='missing00', source='MissingSrc', action=Touch('${TARGET}'))
 
-prev_level = failed_target0 + ok_target0
+prev_level  = failed0 + ok0 + ignore0
+prev_prereq = prereq0
+prev_ignore = ignore0
+prev_igreq  = igreq0
 
 for i in range(1,20):
     
-    failed_target = Command(target='failed%02d' % i, source='', action=fail_action)
-    ok_target     = Command(target='ok%02d' % i,     source='', action=Touch('${TARGET}'))
+    failed = Command(target='failed%02d' % i,  source='', action=fail_action)
+    ok     = Command(target='ok%02d' % i,      source='', action=Touch('${TARGET}'))
+    prereq = Command(target='prereq%02d' % i,  source='', action=Touch('${TARGET}'))
+    ignore = Command(target='ignore%02d' % i,  source='', action=Touch('${TARGET}'))
+    igreq  = Command(target='igreq%02d' % i,   source='', action=Touch('${TARGET}'))
+    missing = Command(target='missing%02d' %i, source='MissingSrc', action=Touch('${TARGET}'))
 
-    next_level = failed_target + ok_target
+    next_level = failed + ok + ignore + igreq + missing
+
     for j in range(1,10):
-        next_level = next_level + Alias('a%02d%02d' % (i,j), prev_level)
+        a = Alias('a%02d%02d' % (i,j), prev_level)
 
-    prev_level = next_level
+        Requires(a, prev_prereq)
+        Ignore(a, prev_ignore)
+
+        Requires(a, prev_igreq)
+        Ignore(a, prev_igreq)
+
+        next_level = next_level + a
+
+    prev_level  = next_level
+    prev_prereq = prereq
+    prev_ignore = ignore
+    prev_igreq  = igreq
 
 all = Alias('all', prev_level)
 
+Requires(all, prev_prereq)
+Ignore(all,  prev_ignore)
+
+Requires(all, prev_igreq)
+Ignore(all,  prev_igreq)
+
 Default(all)
 """)
+
+re_error = """\
+(scons: \\*\\*\\* \\[failed\\d+] Error 2\\n)|\
+(scons: \\*\\*\\* Source `MissingSrc' not found, needed by target `missing\\d+'\\.(  Stop\\.)?\\n)\
+"""
+
+re_errors = "(" + re_error + ")+"
 
 test.run(arguments = 'all',
          status = 2,
@@ -85,7 +124,7 @@ for i in range(5):
 
     test.run(arguments = '-j8 all',
              status = 2,
-             stderr = "(scons: \*\*\* \[failed\d+] Error 2\n)+",
+             stderr = re_errors,
              match=TestSCons.match_re_dotall)
 
 
@@ -94,16 +133,19 @@ for i in range(5):
 
     test.run(arguments = '-j 8 -k all',
              status = 2,
-             stderr = "(scons: \*\*\* \[failed\d+] Error 2\n)+",
+             stderr = re_errors,
              match=TestSCons.match_re_dotall)
     for i in range(20):
         test.must_exist(test.workpath('ok%02d' % i))
+        test.must_exist(test.workpath('prereq%02d' % i))
+        test.must_not_exist(test.workpath('ignore%02d' % i))
+        test.must_exist(test.workpath('igreq%02d' % i))
 
 
 for i in range(5):
     test.run(arguments = 'all --random',
              status = 2,
-             stderr = "scons: \*\*\* \[failed\d+] Error 2\n",
+             stderr = re_errors,
              match=TestSCons.match_re_dotall)
     test.must_not_exist(test.workpath('ok'))
 
@@ -112,7 +154,7 @@ for i in range(5):
 
     test.run(arguments = '-j8 --random all',
              status = 2,
-             stderr = "(scons: \*\*\* \[failed\d+] Error 2\n)+",
+             stderr = re_errors,
              match=TestSCons.match_re_dotall)
 
 for i in range(5):
@@ -120,9 +162,12 @@ for i in range(5):
 
     test.run(arguments = '-j 8 -k --random all',
              status = 2,
-             stderr = "(scons: \*\*\* \[failed\d+] Error 2\n)+",
+             stderr = re_errors,
              match=TestSCons.match_re_dotall)
     for i in range(20):
         test.must_exist(test.workpath('ok%02d' % i))
+        test.must_exist(test.workpath('prereq%02d' % i))
+        test.must_not_exist(test.workpath('ignore%02d' % i))
+        test.must_exist(test.workpath('igreq%02d' % i))
 
 test.pass_test()
