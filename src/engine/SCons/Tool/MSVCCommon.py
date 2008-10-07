c707e396800f6ed5d28fdc8fa23915e77f269da2
David Cournapeau <cournape@developer.scons.org>
2008-10-07 08:50:10 +0000

    Use normalized env when executing the .bat file.

--- src/engine/SCons/Tool/MSVCCommon.py
+++ src/engine/SCons/Tool/MSVCCommon.py
@@ -381,7 +381,8 @@ def ParseBatFile(env, path, vars=['INCLUDE', 'LIB', 'LIBPATH', 'PATH'], args=Non
 
     # XXX: fix args handling here. Do not use a string but a sequence to avoid
     # escaping problems, and letting Popen taking care of it for us.
-    output = get_output(path, args, vars, env=env['ENV'])
+    nenv = normalize_env(env['ENV'], _VSCOMNTOOL_VARNAME.keys() + ['COMSPEC'])
+    output = get_output(path, args, vars, env=nenv)
 
     parsed = parse_output(output, vars)
     ret = {}
