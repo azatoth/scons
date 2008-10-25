f3e076dae625cebee779757abd030f4b6c08167d
David Cournapeau <cournape@developer.scons.org>
2008-10-25 12:25:57 +0000

    Add a function to query available .net frameworks.

--- src/engine/SCons/Tool/MSVCCommon/netframework.py
+++ src/engine/SCons/Tool/MSVCCommon/netframework.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from common import read_reg, debug
 
@@ -18,3 +19,28 @@ def find_framework_root():
         return None
 
     return froot
+
+def query_versions():
+    froot = find_framework_root()
+    if froot:
+        os.listdir(froot)
+
+        l = re.compile('v[0-9]+.*')
+        versions = filter(lambda e, l=l: l.match(e), contents)
+
+        def versrt(a,b):
+            # since version numbers aren't really floats...
+            aa = a[1:]
+            bb = b[1:]
+            aal = string.split(aa, '.')
+            bbl = string.split(bb, '.')
+            # sequence comparison in python is lexicographical
+            # which is exactly what we want.
+            # Note we sort backwards so the highest version is first.
+            return cmp(bbl,aal)
+
+        versions.sort(versrt)
+    else:
+        versions = []
+
+    return versions
