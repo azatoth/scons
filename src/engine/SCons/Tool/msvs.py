ab85607cb55509bdae656d2e57043af7317430a1
William Deegan <bdbaddog@developer.scons.org>
2009-04-30 00:32:43 +0000

    Fix for bug 2405 Also refactored some logic from Defaults._defines() into Defaults.processDefines() which is then used by msvs.py

--- src/engine/SCons/Tool/msvs.py
+++ src/engine/SCons/Tool/msvs.py
@@ -50,6 +50,7 @@ import SCons.Util
 import SCons.Warnings
 
 from MSCommon import msvs_exists, merge_default_version
+from SCons.Defaults import processDefines
 
 ##############################################################################
 # Below here are the classes and functions for generation of
@@ -689,7 +690,7 @@ class _GenerateV7DSP(_DSPGenerator):
             # TODO(1.5)
             #preprocdefs = xmlify(';'.join(self.env.get('CPPDEFINES', [])))
             #includepath = xmlify(';'.join(self.env.get('CPPPATH', [])))
-            preprocdefs = xmlify(string.join(self.env.get('CPPDEFINES', []), ';'))
+            preprocdefs = xmlify(string.join(processDefines(self.env.get('CPPDEFINES', [])), ';'))
             includepath = xmlify(string.join(self.env.get('CPPPATH', []), ';'))
 
             if not env_has_buildtarget:
